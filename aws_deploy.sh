b780ae997a86d044ed9bcebe558afda3658f63cd
Ben Coleman <colemanb@moravian.edu>
2024-04-21 17:21:40 -0400

    add astericks to make the message more prominent

--- aws_deploy.sh
+++ aws_deploy.sh
@@ -4,5 +4,7 @@ sudo systemctl enable docker
 sudo systemctl start docker
 sudo usermod -a -G docker ec2-user
 
+echo "****************************************************"
 echo "Docker running and ec2-user added to docker group."
 echo "Log out and log back in, and then run ./build.sh."
+echo "****************************************************"
