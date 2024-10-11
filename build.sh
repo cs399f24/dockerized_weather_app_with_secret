60cb828bb509b16cf4c8288a3ff1f5c0498f0d1e
Ben Coleman <colemanb@moravian.edu>
2024-10-11 08:25:01 -0400

    remove .env file checks

--- build.sh
+++ build.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# if collector/collector.env does not exist, error out
-if [ ! -f collector/collector.env ]; then
-  echo "collector/collector.env does not exist. Exiting."
-  exit 1
-fi
-
-if [ ! -f server/server.env ]; then
-  echo "server/server.env does not exist. Exiting."
-  exit 1
-fi
-
 docker build -t collector collector
 docker build -t server server
 docker network create weather
